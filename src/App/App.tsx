d059e12138f804508b748821e349952c9a730f16
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-19 11:03:10 -0500

    Adds Material UI AppBar component for nav.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import Navbar from '../components/Navbar/Navbar';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-			<h1 className="black">Black</h1>
-			<h1 className="gray">Gray</h1>
-			<h1 className="lightGray">Light Gray</h1>
-			<h1 className="lighterGray">Lighter Gray</h1>
-			<h1 className="accent">Accent</h1>
-			<Button variant="contained">I am a button</Button>
+			<Navbar />
+			<div className="content">
+				<h1 className="black">Black</h1>
+				<h1 className="gray">Gray</h1>
+				<h1 className="lightGray">Light Gray</h1>
+				<h1 className="lighterGray">Lighter Gray</h1>
+				<h1 className="accent">Accent</h1>
+				<Button variant="contained">I am a button</Button>
+			</div>
     </div>
   );
 }
