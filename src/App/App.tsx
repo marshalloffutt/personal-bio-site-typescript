01274207ffa8ddf63c96c1edd1554d73fc4f7f22
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-19 12:50:23 -0500

    Adds layout for sections.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import Button from "@mui/material/Button";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
+import Home from "../components/Home/Home";
+import About from "../components/About/About";
+import Projects from "../components/Projects/Projects";
+import Contact from "../components/Contact/Contact";
 import "./App.scss";
 
 function App() {
@@ -9,6 +13,10 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
         <h1 className="black">Black</h1>
         <h1 className="gray">Gray</h1>
         <h1 className="lightGray">Light Gray</h1>
@@ -16,7 +24,7 @@ function App() {
         <h1 className="accent">Accent</h1>
         <Button variant="contained">I am a button</Button>
       </div>
-			<Footer />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,5 @@
+import "./Home.scss";
+
+export default function Home() {
+  return <h1>Home</h1>;
+}
