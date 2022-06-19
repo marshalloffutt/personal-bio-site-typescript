43785ebda10a01962ea587b4d10d0287e1f17813
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-19 12:53:46 -0500

    Adds final colors to scheme.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -21,7 +21,8 @@ function App() {
         <h1 className="gray">Gray</h1>
         <h1 className="lightGray">Light Gray</h1>
         <h1 className="lighterGray">Lighter Gray</h1>
-        <h1 className="accent">Accent</h1>
+        <h1 className="red">Red</h1>
+        <h1 className="green">Green</h1>
         <Button variant="contained">I am a button</Button>
       </div>
       <Footer />
