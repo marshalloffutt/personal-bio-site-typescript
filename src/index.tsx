8ccf4d1dec4563ac93c7bf202744845821f4fe91
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-18 22:10:36 -0500

    Reorganize, and import dev dependencies.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
