afcf341cc40d1e35330d83b0d3ca5b4ddd8316e7
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-19 12:28:52 -0500

    Adds basic empty footer component.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Navbar from '../components/Navbar/Navbar';
-import './App.scss';
+import React from "react";
+import Button from "@mui/material/Button";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-			<Navbar />
-			<div className="content">
-				<h1 className="black">Black</h1>
-				<h1 className="gray">Gray</h1>
-				<h1 className="lightGray">Light Gray</h1>
-				<h1 className="lighterGray">Lighter Gray</h1>
-				<h1 className="accent">Accent</h1>
-				<Button variant="contained">I am a button</Button>
-			</div>
+      <Navbar />
+      <div className="content">
+        <h1 className="black">Black</h1>
+        <h1 className="gray">Gray</h1>
+        <h1 className="lightGray">Light Gray</h1>
+        <h1 className="lighterGray">Lighter Gray</h1>
+        <h1 className="accent">Accent</h1>
+        <Button variant="contained">I am a button</Button>
+      </div>
+			<Footer />
     </div>
   );
 }
