6bad31b77f28d61c49544927e85ac30ced6ecfa3
Marshall Offutt <marshalloffutt@gmail.com>
2022-06-26 17:23:06 -0500

    Deploy to firebase.

---
+++ src/helpers/data/connection.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+import apiKeys from "../apiKeys";
+
+const firebaseApp = () => {
+  initializeApp(apiKeys.firebaseConfig);
+};
+
+export default firebaseApp;
