9939ae57a1751bf434da506dcc11e38fe7e24cd5
Marshall Offutt <marshalloffutt@Marshalls-Mac-mini.local>
2022-08-20 14:43:28 -0500

    Tweak formatting.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,21 +6,21 @@ export default function About() {
   let years: number = moment().diff("2019-08-01", "years");
 
   const ABOUT_ME_TEXT_1: string = `Technology has always been an obsession of mine. As
-	someone who appreciates well-thought-out design, loves to build things, and has a
-	passion for	learning, it became obvious that software development was the perfect
-	career - and hobby - to induldge my curious nature and penchant for tinkering.`;
+    someone who appreciates well-thought-out design, loves to build things, and has a
+    passion for	learning, it became obvious that software development was the perfect
+    career - and hobby - to induldge my curious nature and penchant for tinkering.`;
 
   const ABOUT_ME_TEXT_2: string = `I am currently employed as a Software Engineer at
-	${EMPLOYER_NAME}. With over ${years} years of enterprise-level experience under my
-	belt, I can honestly say that I love what I do! Few things in life are more
-	satisfying than collaborating and teambuilding within a shared goal of building great
-	software.`;
+    ${EMPLOYER_NAME}. With over ${years} years of enterprise-level experience under my
+    belt, I can honestly say that I love what I do! Few things in life are more
+    satisfying than collaborating and teambuilding within a shared goal of building great
+    software.`;
 
   const ABOUT_ME_TEXT_3: string = `Outside of work, I try to keep an active and healthy
-	lifestyle. I'm a lifelong runner who also loves hiking with my wife and dogs.
-	For fun I love traveling, and screaming until I'm hoarse at Nashville SC matches... or just
-	relaxing with some video games. I also make it a point to set aside personal time to keep up
-	with trends in the IT field, learning new technologies and coding personal projects.`;
+    lifestyle. I'm a lifelong runner who also loves hiking with my wife and dogs.
+    For fun I love traveling, and screaming until I'm hoarse at Nashville SC matches... or just
+    relaxing with some video games. I also make it a point to set aside personal time to keep up
+    with trends in the IT field, learning new technologies and coding personal projects.`;
 
   return (
     <div className="about-me-container" id="AboutMe">
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,8 +6,8 @@ import { EMPLOYER_NAME } from "../../lib/constants";
 const HEADER_TEXT: string = "Marshall Offutt";
 const TITLE_TEXT: string = "Full-Stack Developer";
 const BLURB_TEXT: string = `Nashville-based software developer with a focus on ASP.NET
-Core and React. Passionate about continuing education, and real-world problem
-solving. Currently working as a Software Engineer at ${EMPLOYER_NAME}.`;
+    Core and React. Passionate about continuing education, and real-world problem
+    solving. Currently working as a Software Engineer at ${EMPLOYER_NAME}.`;
 
 export default function Home() {
   return (
